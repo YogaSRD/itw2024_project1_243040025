b027fd9d4cd0c1137d2ca054adbf75d4e3f77683
YogaSRD <yogaapry608@gmail.com>
2025-01-07 05:08:19 +0700

    Tampilan 1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Toggle the navbar menu
+const toggler = document.getElementById("navbar-toggler");
+const links = document.getElementById("navbar-links");
+
+toggler.addEventListener("click", () => {
+  links.classList.toggle("show");
+});
