643b5297878fb7df303375ecb3c7925dd85b3d28
YogaSRD <yogaapry608@gmail.com>
2025-01-07 09:18:55 +0700

    perbaikan akhir

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// Toggle the navbar menu
 const toggler = document.getElementById("navbar-toggler");
 const links = document.getElementById("navbar-links");
 
-// Menambahkan event listener untuk tombol hamburger
 toggler.addEventListener("click", () => {
-  links.classList.toggle("show"); // Toggle kelas "show" pada navbar-links
+  links.classList.toggle("show");
 });
