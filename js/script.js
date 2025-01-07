b7e004d929f38d434f2586c943f86abfde149b7b
YogaSRD <yogaapry608@gmail.com>
2025-01-07 07:50:22 +0700

    memperbaiki fungsi navbar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const toggler = document.getElementById("navbar-toggler");
 const links = document.getElementById("navbar-links");
 
+// Menambahkan event listener untuk tombol hamburger
 toggler.addEventListener("click", () => {
-  links.classList.toggle("show");
+  links.classList.toggle("show"); // Toggle kelas "show" pada navbar-links
 });
